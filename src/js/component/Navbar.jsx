48d8d6e187edee65f98ec03095b97f894ac6c57e
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-02-13 21:21:57 +0000

    listo

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Hamburger from 'hamburger-react';
 
 
 
@@ -18,10 +19,22 @@ function NavBar() {
 
           <div className="">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link className='d-none d-sm-block ' href="#home">Home</Nav.Link>
+            <Nav.Link className='d-none d-sm-block' href="#services">Services</Nav.Link>
+            <Nav.Link className='d-none d-sm-block' href="#about">About Us</Nav.Link>
+            <Nav.Link className='d-none d-sm-block' href="#contact">Contact</Nav.Link>
+
+            <div className="dropdown d-block d-sm-none " >
+            <a className="btn btn-dark dropdown-toggle bg-dark d-flex "  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Hamburger hideOutline={false} size={20} />
+            </a>
+
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Home</a></li>
+              <li><a className="dropdown-item" href="#">Services</a></li>
+              <li><a className="dropdown-item" href="#">About Us</a></li>
+            </ul>
+          </div>
           </Nav>
           </div>
         </Container>
